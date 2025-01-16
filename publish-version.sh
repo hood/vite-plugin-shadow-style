cefb7a2f9ec21c6e9dc73270ebc62af4bba129fc
Andrea Cappuccio <andrea.cappuccio@moneyfarm.com>
2025-01-16 18:34:03 +0100

    v1.1.2

--- publish-version.sh
+++ publish-version.sh
@@ -2,8 +2,18 @@
 
 read -p "Enter the version you want to publish: " version
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' "s/\"version\": \".*\"/\"version\": \"$version\"/gi" ./package.json
+else
+  sed -i "s/\"version\": \".*\"/\"version\": \"$version\"/gi" ./package.json
+fi
+
 git add .
+
 git commit -m "v$version"
+
 git tag "v$version"
+
 git push --tags
+
 npm publish
